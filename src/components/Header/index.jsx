7e7104e7e5db1607077a3818c3696740784093a1
Amin Houssni <houssni.amin@outlook.fr>
2025-01-20 14:52:36 +0100

    Created header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react"
+import "./header.css"
+import whiteLogo from "../../assets/clearLogo.svg"
+import blackLogo from "../../assets/darkLogo.svg"
+import darkAccount from "../../assets/darkAccount.svg"
+import clearAccount from "../../assets/clearAccount.svg"
+
+export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
+  return (
+    <header className={`${isScrolled ? "headerScrolled" : ""}`}>
+      <div className="container">
+        <img src={isScrolled ? blackLogo : whiteLogo} alt="Logo Motto" />
+        <div>
+          <a className={`link ${isScrolled ? "linkScrolled" : ""}`} href="/">
+            SERVICES
+          </a>
+          <a className={`link ${isScrolled ? "linkScrolled" : ""}`} href="/">
+            E-BIKES
+          </a>
+          <a className={`link ${isScrolled ? "linkScrolled" : ""}`} href="/">
+            À PROPOS
+          </a>
+          <a
+            className={`link ${isScrolled ? "linkScrolled" : ""}`}
+            href="https://blog.ridemotto.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            BLOG
+          </a>
+        </div>
+        <div className="btnHader">
+          <button
+            className={`subscribeBtn 
+              ${isScrolled ? "subscribeBtnScrolled" : ""}`}
+          >
+            S'ABONNER
+          </button>
+          <img
+            src={isScrolled ? clearAccount : darkAccount}
+            alt="logo account"
+            className={`logoAccount 
+              ${isScrolled ? "logoAccountScrolled" : ""}`}
+          />
+        </div>
+      </div>
+    </header>
+  )
+}
