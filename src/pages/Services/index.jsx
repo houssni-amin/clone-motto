cee1bed90237a587db523f847db37d7fe7e1f6da
Amin Houssni <houssni.amin@outlook.fr>
2025-01-22 16:50:33 +0100

    Created the second section of the Services page

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -3,6 +3,9 @@ import Header from "../../components/Header"
 import blackLogo from "../../assets/darkLogo.svg"
 import whiteAccount from "../../assets/clearAccount.svg"
 import bikeTop from "../../assets/bikeTop.avif"
+import modelLeft from "../../assets/modelLeft.svg"
+import modelCenter from "../../assets/modelCenter.svg"
+import modelRight from "../../assets/modelRight.svg"
 
 export default function Services() {
   return (
@@ -31,6 +34,48 @@ export default function Services() {
         </p>
         <button>DÉCOUVRIR</button>
         <img src={bikeTop} alt="Vélo" />
+      </div>
+
+      <div className="secondServices">
+        <h2>
+          DES SERVICES PENSÉS
+          <br />
+          POUR VOTRE TRANQUILITÉD'ESPRIT
+        </h2>
+        <p>Disponibles pour tous nos modèles</p>
+        <div className="modelsContainer">
+          <div className="model">
+            <img src={modelLeft} alt="" />
+            <h3>
+              ASSURANCE CONTRE
+              <br /> LE VOL
+            </h3>
+            <p>
+              Roulez l'esprit tranquille avec notre assurance <br />
+              contre le vol
+            </p>
+          </div>
+          <div className="model">
+            <img src={modelCenter} alt="" />
+            <h3>
+              MAINTENANCE & <br />
+              RÉPARATIONS
+            </h3>
+            <p>
+              Accéder à notre réseau de partenaires à domicile pour vous
+              simplifier la vie
+            </p>
+          </div>
+          <div className="model">
+            <img src={modelRight} alt="" />
+            <h3>TRACEUR & ÉQUIPE DE RECHERCHE</h3>
+            <p>
+              Localisation en cas de vol grâce à notre traceur intégré et notre
+              équipe dédiée
+            </p>
+          </div>
+        </div>
+        <button>COMPARER NOS OFFRES</button>
       </div>
     </main>
   )
