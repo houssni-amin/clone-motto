b26380f825d183f739ec099750d15b41d5bf0e54
Amin Houssni <houssni.amin@outlook.fr>
2025-01-22 16:49:40 +0100

    Created the first section of the Services page

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -2,6 +2,7 @@ import "./services.css"
 import Header from "../../components/Header"
 import blackLogo from "../../assets/darkLogo.svg"
 import whiteAccount from "../../assets/clearAccount.svg"
+import bikeTop from "../../assets/bikeTop.avif"
 
 export default function Services() {
   return (
@@ -20,6 +21,17 @@ export default function Services() {
         logoAccountDefault={whiteAccount}
         logoAccountScroll={whiteAccount}
       />
+
+      <div className="firstServices">
+        <h1>DÉCOUVREZ NOS SERVICES</h1>
+        <p>
+          Où que votre vélo vous emmène, nous vous accompagnons.
+          <br /> Avec notre gamme de services, nos sommes là à tous les coins de
+          rue.
+        </p>
+        <button>DÉCOUVRIR</button>
+        <img src={bikeTop} alt="Vélo" />
+      </div>
     </main>
   )
 }
